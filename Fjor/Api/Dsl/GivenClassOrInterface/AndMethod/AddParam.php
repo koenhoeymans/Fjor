75fcca96b74a62998c1450b891c6f845a67cb43e
koenhoeymans <koenhoeymans@gmail.com>
2014-04-16 07:32:55 +0200

    improve PhpDoc

--- Fjor/Api/Dsl/GivenClassOrInterface/AndMethod/AddParam.php
+++ Fjor/Api/Dsl/GivenClassOrInterface/AndMethod/AddParam.php
@@ -5,6 +5,9 @@ namespace Fjor\Api\Dsl\GivenClassOrInterface\AndMethod;
 interface AddParam
 {
 	/**
+	 * Specifies the values that should be injected in a method. `addParam`
+	 * can be called more than once to inject other values.
+	 * 
 	 * @param array $values
 	 * 
 	 * @return AddParam
