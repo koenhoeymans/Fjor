218c7da1a4690397b05cfdc6203fdb28486bf2c5
koenhoeymans <koenhoeymans@gmail.com>
2014-04-23 16:12:51 +0200

    refactoring

--- Fjor/ObjectFactory/GenericObjectFactory.php
+++ Fjor/ObjectFactory/GenericObjectFactory.php
@@ -7,128 +7,117 @@ Use Fjor\Injection\InjectionMap;
 
 class GenericObjectFactory implements ObjectFactory
 {
-	public function createInstance($class, InjectionMap $injections, ObjectGraphConstructor $ogc)
-	{	
-		## we'll have to create a new instance
-		$reflectionClass = new \ReflectionClass($class);
-	
-		if (!$reflectionClass->hasMethod('__construct'))
-		{
+	private $ogc;
+
+	public function createInstance(
+		$class,
+		InjectionMap $injectionMap,
+		ObjectGraphConstructor $ogc
+	) {
+		$this->ogc = $ogc;
+
+		$obj = $this->createObject($class, $injectionMap);
+		$this->injectMethodDependencies($obj, $injectionMap);
+
+		return $obj;
+	}
+
+	private function createObject($className, InjectionMap $injectionMap)
+	{
+		$reflectionClass = new \ReflectionClass($className);
+		if (!$reflectionClass->hasMethod('__construct')) {
 			$obj = $reflectionClass->newInstanceArgs();
 		}
-		else
-		{
-			$params = $injections->getParams('__construct');
-			$params = $this->findMissingDependencies(
-				$params[0],
-				$reflectionClass->getMethod('__construct'),
-				$ogc
+		else {
+			$userSpecifiedArgs = $injectionMap->getParams('__construct')[0];
+			$args = $this->getMethodDependencies(
+				$userSpecifiedArgs,
+				$reflectionClass->getMethod('__construct')
 			);
-
-			$obj = $reflectionClass->newInstanceArgs($params);
-		}
-
-		foreach ($injections->getMethods() as $method)
-		{
-			if ($method === '__construct')
-			{
-				continue;
-			}
-
-			$reflectionMethod = $reflectionClass->getMethod($method);
-			foreach ($injections->getParams($method) as $params)
-			{
-				$params = $this->findMissingDependencies(
-					$params, $reflectionMethod, $ogc
-				);
-				call_user_func_array(array($obj, $method), $params);
-			}
+			$obj = $reflectionClass->newInstanceArgs($args);
 		}
 
 		return $obj;
 	}
 
-	private function findMissingDependencies(
-		array $params, \ReflectionMethod $reflectionMethod, ObjectGraphConstructor $ogc
-	) {
-		foreach ($reflectionMethod->getParameters() as $reflectionParameter)
-		{
-			$argumentPosition = $reflectionParameter->getPosition();
-			$paramReflectionClass = $reflectionParameter->getClass();
-			$value = isset($params[$argumentPosition]) ?
-				$params[$argumentPosition] : null;
-
-			# an object needed
-			if ($paramReflectionClass)
-			{
-				$paramObj = $this->getObject($value, $paramReflectionClass, $ogc);
-			}
-			# something else needed and it's specified
-			elseif (isset($value))
-			{
+	private function injectMethodDependencies($object, InjectionMap $injectionMap)
+	{
+		$reflectionClass = new \ReflectionClass($object);
+		foreach ($injectionMap->getMethods() as $method) {
+			if ($method === '__construct') {
 				continue;
 			}
-			# something else needed, it's not specified but default available
-			elseif ($reflectionParameter->isDefaultValueAvailable())
-			{
+
+			$reflectionMethod = $reflectionClass->getMethod($method);
+			foreach ($injectionMap->getParams($method) as $userSpecifiedArgs) {
+				$params = $this->getMethodDependencies(
+					$userSpecifiedArgs,
+					$reflectionMethod
+				);
+				call_user_func_array(array($object, $method), $params);
+			}
+		}
+	}
+
+	private function getMethodDependencies(
+		array $userSpecifiedArgs,
+		\ReflectionMethod $reflectionMethod
+	) {
+		foreach ($reflectionMethod->getParameters() as $reflectionParameter) {
+			$argumentPosition = $reflectionParameter->getPosition();
+			$userSpecifiedValue = isset($userSpecifiedArgs[$argumentPosition])
+				? $userSpecifiedArgs[$argumentPosition]
+				: null;
+
+			if (is_object($userSpecifiedValue)) {
+				continue;
+			}
+			elseif ($reflectionParameter->getClass()) {
+				$paramObj = $this->getObject(
+					$userSpecifiedValue,
+					$reflectionParameter->getClass()->getName()
+				);
+			}
+			elseif (isset($userSpecifiedValue)) {
+				continue;
+			}
+			elseif ($reflectionParameter->isDefaultValueAvailable()) {
 				$paramObj = $reflectionParameter->getDefaultValue();
 			}
-			# we can get away with `null` as value
-			elseif ($reflectionParameter->allowsNull())
-			{
+			elseif ($reflectionParameter->allowsNull()) {
 				$paramObj = null;
 			}
-			# a last resort
-			# there's no default value for internal functions though the manual
-			# often states 'null'. Eg SplObjectStorage::attach($obj, $optional = null)
-			# Here we can be saved by using `IsOptional`.
-			elseif ($reflectionParameter->isOptional())
-			{
+			elseif ($reflectionParameter->isOptional()) {
 				continue;
 			}
-			else
-			{
+			else {
 				$class = $reflectionMethod->getDeclaringClass()->getName();
 				$method = $reflectionMethod->getName();
 				$param = $reflectionParameter->getName();
 				throw new \Exception(
 					'No dependency specified for "' . $class . '::' . $method
-					. '" on position ' . $argumentPosition . ', parametername $' . $param
+					. '" on position ' . $argumentPosition . ', parametername $'
+					. $param
 				);
 			}
 
-			$params[$argumentPosition] = $paramObj;
+			$userSpecifiedArgs[$argumentPosition] = $paramObj;
 		}
 
-		ksort($params);
+		ksort($userSpecifiedArgs);
 
-		return $params;
+		return $userSpecifiedArgs;
 	}
 
 	private function getObject(
-		$value, \ReflectionClass $paramReflectionClass, ObjectGraphConstructor $ogc
+		$userGivenClassName = null,
+		$paramClassOrInterfaceName
 	) {
-		## provided by user
-		if ($value)
-		{
-			### as binding
-			if (is_string($value))
-			{
-				$paramObj = $ogc->getInstance($value);
-			}
-			### or object
-			else
-			{
-				$paramObj = $value;
-			}
+		if ($userGivenClassName) {
+			return $this->ogc->getInstance($userGivenClassName);
 		}
-		## not provided by user -> get it from Fjor
-		else
-		{
-			$paramClassName = $paramReflectionClass->getName();
-			$paramObj = $ogc->getInstance($paramClassName);
+		else {
+			return $this->ogc->getInstance($paramClassOrInterfaceName);
 		}
-
-		return $paramObj;
 	}
 }
